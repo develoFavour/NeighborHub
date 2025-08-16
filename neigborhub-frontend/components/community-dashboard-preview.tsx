fc0b72328d52433bd3639f78d98a8c0d6a4fa732
opia favour <opiafavour10@gmail.com>
2025-08-16 09:46:39 +0100

    feat:(backend folder structure and dependencies installation

--- neigborhub-frontend/components/community-dashboard-preview.tsx
+++ neigborhub-frontend/components/community-dashboard-preview.tsx
@@ -1,17 +1,17 @@
-import Image from "next/image"
+import Image from "next/image";
 
 export function CommunityDashboardPreview() {
-  return (
-    <div className="w-[calc(100vw-32px)] md:w-[900px]">
-      <div className="bg-primary-light/30 rounded-2xl p-2 shadow-2xl">
-        <Image
-          src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-nPvG45vZfy64WDs0jb4vWARfzjejCe.png"
-          alt="Community sharing dashboard preview"
-          width={900}
-          height={540}
-          className="w-full h-full object-cover rounded-xl shadow-lg"
-        />
-      </div>
-    </div>
-  )
+	return (
+		<div className="w-[calc(100vw-50px)] md:w-[72rem]">
+			<div className="bg-primary-light/30 rounded-2xl p-2 shadow-2xl">
+				<Image
+					src="/assets/neigbor.jpg"
+					alt="Community sharing dashboard preview"
+					width={900}
+					height={540}
+					className="w-full h-full object-cover rounded-xl shadow-lg"
+				/>
+			</div>
+		</div>
+	);
 }
